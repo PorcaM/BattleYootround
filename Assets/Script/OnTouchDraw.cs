83e3a48094e91ca3487a95105954fd56d8176622
98765kjh <98765kjh@gmail.com>
2017-10-30 21:24:06 +0900

    commit test

---
+++ Assets/Script/OnTouchDraw.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTouchDraw : MonoBehaviour {
+
+    public Texture2D heightmap;
+    public Vector3 size = new Vector3(100, 10, 100);
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        int x = Mathf.FloorToInt(transform.position.x / size.x * heightmap.width);
+        int z = Mathf.FloorToInt(transform.position.z / size.z * heightmap.height);
+        Vector3 pos = transform.position;
+        pos.y = heightmap.GetPixel(x, z).grayscale * size.y;
+        Debug.Log(pos);
+        Debug.Log(heightmap.GetPixels());
+    }
+}
