c430898fba326ea652f4ed6dd7de60b2319fef98
sj lee <tjdwns5122@gmail.com>
2017-11-01 19:01:53 +0900

    Init PlayerProfile
    시작할때 플레이어 프로필 보임 (레벨1 고정)
    파일 읽지 않음
    다음 장면으로 넘어가지 않음

---
+++ Assets/Script/LogPlayerProfile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LogPlayerProfile : MonoBehaviour {
+    public PlayerProfile playerProfile;
+    private Text textObject;
+
+	// Use this for initialization
+	void Start () {
+        textObject = GetComponent<UnityEngine.UI.Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        textObject.text = "PlayerProfile\n" + "Level: " + playerProfile.level;
+	}
+}
