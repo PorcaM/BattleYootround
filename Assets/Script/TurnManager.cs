095d3324f194f5cc317d28c0cc42918232e0bfb8
sj lee <tjdwns5122@gmail.com>
2017-11-06 03:04:18 +0900

    Init Turn Manager

---
+++ Assets/Script/TurnManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnManager : MonoBehaviour
+{
+    public enum YootCount { Nak, Do, Gae, Gul, Yoot, Mo, BackDo = 1 };
+    private GameObject[] horseObjects;
+
+    void Start()
+    {
+        horseObjects = GameObject.FindGameObjectsWithTag("AllyHorse");
+    }
+
+    void ProceedTurn()
+    {
+        YootCount yootCount = ThrowYoot();
+        if (yootCount == YootCount.Nak)
+            return;
+        else
+            MoveHorse();
+    }
+
+    private YootCount ThrowYoot()
+    {
+        return YootCount.Do;
+    }
+
+    private void MoveHorse()
+    {
+
+    }
+}
