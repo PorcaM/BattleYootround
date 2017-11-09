f07bb21a890c7288c51e6b9237a08c7cc44ad5ce
sj lee <tjdwns5122@gmail.com>
2017-11-10 01:28:42 +0900

    Init YootFieldFactory
    윷 필드의 초기화(생산)은 팩토리가 담당

--- Assets/Script/YootBoard.cs
+++ Assets/Script/YootBoard.cs
@@ -4,21 +4,7 @@ using UnityEngine;
 
 public class YootBoard : MonoBehaviour {
     public GameObject fieldPref;
-    public const int YootFieldCount = 29;
-    private static List<YootField> fields;
-
-    public static List<YootField> Fields
-    {
-        get
-        {
-            return fields;
-        }
-
-        set
-        {
-            fields = value;
-        }
-    }
+    public List<GameObject> fields;
 
     void Start()
     {
@@ -27,18 +13,6 @@ public class YootBoard : MonoBehaviour {
 
     private void Init()
     {
-        Fields = new List<YootField>();
-        for (int i = 0; i < YootFieldCount; i++)
-        {
-            float x = i;
-            float y = 0;
-            Vector3 pos = new Vector3(x, y, 0);
-            GameObject newObject = Instantiate(fieldPref, pos, Quaternion.identity, transform) as GameObject;
-            Fields.Add(newObject.GetComponent<YootField>());
-        }
-        for (int i = 0; i < YootFieldCount; i++)
-        {
-            Fields[i].Init(i);
-        }
+        fields = YootFieldFactory.CreateYootFields(fieldPref, transform);
     }
 }
--- Assets/Script/YootField.cs
+++ Assets/Script/YootField.cs
@@ -4,10 +4,7 @@ using UnityEngine;
 
 public class YootField : MonoBehaviour {
     private int id;
-    public YootField back;
-    public YootField next;
-    public YootField side;
-    private const int outsideCount = 20;
+    private List<Horse> horses;
 
     public int Id
     {
@@ -22,79 +19,27 @@ public class YootField : MonoBehaviour {
         }
     }
 
+    public List<Horse> Horses
+    {
+        get
+        {
+            return horses;
+        }
+
+        set
+        {
+            horses = value;
+        }
+    }
+
     void Start()
     {
         Id = -1;
-        back = next = side = null;
+        Horses = new List<Horse>();
     }
 
     public void Init(int id)
     {
-        Debug.Log(id);
         Id = id;
-        if (IsOutside(Id))
-            OutsideInit();
-        else
-            InsideInit();
-    }
-
-    private static bool IsOutside(int id)
-    {
-        return id < outsideCount;
-    }
-
-    private void OutsideInit()
-    {
-        InitBackNext();
-        if (IsWithBypass())
-            InitSide();
-    }
-
-    private void InitBackNext()
-    {
-        back = YootBoard.Fields[(Id + YootBoard.YootFieldCount - 1) % outsideCount];
-        next = YootBoard.Fields[(Id + 1) % outsideCount];
-    }
-
-    private bool IsWithBypass()
-    {
-        return Id == 5 || Id == 10 || Id == 22;
-    }
-
-    private void InitSide()
-    {
-        if(Id == 5)
-        {
-            side = YootBoard.Fields[20];
-        }
-        if (Id == 10)
-        {
-            side = YootBoard.Fields[25];
-        }
-        if (Id == 22)
-        {
-            side = YootBoard.Fields[28];
-        }
-    }    
-
-    private void InsideInit()
-    {
-        OutsideInit();
-        if (Id == 20)
-        {
-            back = YootBoard.Fields[5];
-        }
-        if (Id == 24)
-        {
-            next = YootBoard.Fields[15];
-        }
-        if (Id == 25)
-        {
-            back = YootBoard.Fields[10];
-        }
-        if (Id == 28)
-        {
-            next = YootBoard.Fields[0];
-        }
     }
 }
