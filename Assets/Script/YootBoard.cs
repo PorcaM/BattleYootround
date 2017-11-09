ae5419638b3b0a9858c70812ee77ad9a36805fa1
sj lee <tjdwns5122@gmail.com>
2017-11-10 00:54:03 +0900

    Init yootboard and field

---
+++ Assets/Script/YootBoard.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YootBoard : MonoBehaviour {
+    public GameObject fieldPref;
+    public const int YootFieldCount = 29;
+    private static List<YootField> fields;
+
+    public static List<YootField> Fields
+    {
+        get
+        {
+            return fields;
+        }
+
+        set
+        {
+            fields = value;
+        }
+    }
+
+    void Start()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        Fields = new List<YootField>();
+        for (int i = 0; i < YootFieldCount; i++)
+        {
+            float x = i;
+            float y = 0;
+            Vector3 pos = new Vector3(x, y, 0);
+            GameObject newObject = Instantiate(fieldPref, pos, Quaternion.identity, transform) as GameObject;
+            Fields.Add(newObject.GetComponent<YootField>());
+        }
+        for (int i = 0; i < YootFieldCount; i++)
+        {
+            Fields[i].Init(i);
+        }
+    }
+}
--- Assets/Script/YootField.cs
+++ Assets/Script/YootField.cs
@@ -3,14 +3,98 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class YootField : MonoBehaviour {
+    private int id;
+    public YootField back;
+    public YootField next;
+    public YootField side;
+    private const int outsideCount = 20;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public int Id
+    {
+        get
+        {
+            return id;
+        }
+
+        set
+        {
+            id = value;
+        }
+    }
+
+    void Start()
+    {
+        Id = -1;
+        back = next = side = null;
+    }
+
+    public void Init(int id)
+    {
+        Debug.Log(id);
+        Id = id;
+        if (IsOutside(Id))
+            OutsideInit();
+        else
+            InsideInit();
+    }
+
+    private static bool IsOutside(int id)
+    {
+        return id < outsideCount;
+    }
+
+    private void OutsideInit()
+    {
+        InitBackNext();
+        if (IsWithBypass())
+            InitSide();
+    }
+
+    private void InitBackNext()
+    {
+        back = YootBoard.Fields[(Id + YootBoard.YootFieldCount - 1) % outsideCount];
+        next = YootBoard.Fields[(Id + 1) % outsideCount];
+    }
+
+    private bool IsWithBypass()
+    {
+        return Id == 5 || Id == 10 || Id == 22;
+    }
+
+    private void InitSide()
+    {
+        if(Id == 5)
+        {
+            side = YootBoard.Fields[20];
+        }
+        if (Id == 10)
+        {
+            side = YootBoard.Fields[25];
+        }
+        if (Id == 22)
+        {
+            side = YootBoard.Fields[28];
+        }
+    }    
+
+    private void InsideInit()
+    {
+        OutsideInit();
+        if (Id == 20)
+        {
+            back = YootBoard.Fields[5];
+        }
+        if (Id == 24)
+        {
+            next = YootBoard.Fields[15];
+        }
+        if (Id == 25)
+        {
+            back = YootBoard.Fields[10];
+        }
+        if (Id == 28)
+        {
+            next = YootBoard.Fields[0];
+        }
+    }
 }
