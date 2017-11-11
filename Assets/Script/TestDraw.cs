23222b39175b04d1ae793c03e8482390828eecd5
98765kjh <98765kjh@gmail.com>
2017-11-11 19:02:32 +0900

    Modify NullReferenceProblem

--- Assets/Script/TestDraw.cs
+++ Assets/Script/TestDraw.cs
@@ -11,14 +11,44 @@ public class TestDraw : MonoBehaviour {
     void Start()
     {
         objPlane = new Plane(Camera.main.transform.forward, this.transform.position);
-        Debug.Log(this.transform.position);
     }
 
-    void Update () {
+    void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            thisTrail = (GameObject)Instantiate(trailPrefab,
+                                                    this.transform.position,
+                                                    Quaternion.identity);
+
+            Ray mRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float rayDistance;
+            if (objPlane.Raycast(mRay, out rayDistance))
+            {
+                startPos = mRay.GetPoint(rayDistance);
+            }
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            Ray mRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float rayDistance;
+            if (objPlane.Raycast(mRay, out rayDistance))
+            {
+                //Debug.Log(thisTrail);
+                thisTrail.transform.position = mRay.GetPoint(rayDistance);
+            }
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("Ended");
+            Debug.Log(thisTrail);
+            if (Vector3.Distance(thisTrail.transform.position, startPos) < 0.1)
+                Destroy(thisTrail);
+        }
+        /*
         if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
             || Input.GetMouseButtonDown(0))
         {
-            Debug.Log("Began");
             thisTrail = (GameObject)Instantiate(trailPrefab,
                                                     this.transform.position,
                                                     Quaternion.identity);
@@ -28,28 +58,27 @@ public class TestDraw : MonoBehaviour {
             if (objPlane.Raycast(mRay, out rayDistance))
             { 
                 startPos = mRay.GetPoint(rayDistance);
-                Debug.Log(rayDistance);
             }
         }
         else if (((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
              || Input.GetMouseButton(0)))
         {
-            Debug.Log("Moved");
             Ray mRay = Camera.main.ScreenPointToRay(Input.mousePosition);
             float rayDistance;
             if (objPlane.Raycast(mRay, out rayDistance))
             {
+                //Debug.Log(thisTrail);
                 thisTrail.transform.position = mRay.GetPoint(rayDistance);
-                Debug.Log(rayDistance);
             }
         }
         else if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
             || Input.GetMouseButtonUp(0))
         {
             Debug.Log("Ended");
+            Debug.Log(thisTrail);
             if (Vector3.Distance(thisTrail.transform.position, startPos) < 0.1)
                 Destroy(thisTrail);
         }
-
+        */
     }
 }
