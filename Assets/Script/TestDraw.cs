d24f046d1e6d778aa4ad5cb601b7d711b06b4ab5
98765kjh <98765kjh@gmail.com>
2017-11-11 18:49:11 +0900

    multi-draw, exist NullReferenceException Problem

--- Assets/Script/TestDraw.cs
+++ Assets/Script/TestDraw.cs
@@ -3,16 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TestDraw : MonoBehaviour {
-	void Update () {
-		if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) 
-            || Input.GetMouseButton(0))
+    public GameObject trailPrefab;
+    GameObject thisTrail;
+    Vector3 startPos;
+    Plane objPlane;
+
+    void Start()
+    {
+        objPlane = new Plane(Camera.main.transform.forward, this.transform.position);
+        Debug.Log(this.transform.position);
+    }
+
+    void Update () {
+        if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            || Input.GetMouseButtonDown(0))
         {
-            Plane objPlane = new Plane(Camera.main.transform.forward * -1, this.transform.position);
+            Debug.Log("Began");
+            thisTrail = (GameObject)Instantiate(trailPrefab,
+                                                    this.transform.position,
+                                                    Quaternion.identity);
 
             Ray mRay = Camera.main.ScreenPointToRay(Input.mousePosition);
             float rayDistance;
             if (objPlane.Raycast(mRay, out rayDistance))
-                this.transform.position = mRay.GetPoint(rayDistance);
+            { 
+                startPos = mRay.GetPoint(rayDistance);
+                Debug.Log(rayDistance);
+            }
         }
-	}
+        else if (((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+             || Input.GetMouseButton(0)))
+        {
+            Debug.Log("Moved");
+            Ray mRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float rayDistance;
+            if (objPlane.Raycast(mRay, out rayDistance))
+            {
+                thisTrail.transform.position = mRay.GetPoint(rayDistance);
+                Debug.Log(rayDistance);
+            }
+        }
+        else if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+            || Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("Ended");
+            if (Vector3.Distance(thisTrail.transform.position, startPos) < 0.1)
+                Destroy(thisTrail);
+        }
+
+    }
 }
