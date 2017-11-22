12a40c743559b2f4ecf63cb615acefafede15f69
98765kjh <98765kjh@gmail.com>
2017-11-23 00:50:52 +0900

    Reset commit for Delete sound file...

--- Assets/Script/Network/BYMessage.cs
+++ Assets/Script/Network/BYMessage.cs
@@ -4,29 +4,36 @@ using UnityEngine;
 using UnityEngine.Networking;
 
 public class BYMessage : MonoBehaviour {
-
-    public class MyMessage : MessageBase
+    public class EmptyMessage : MessageBase
     {
         public string str;
     }
+    public class HorseMessage : MessageBase
+    {
+        public int horseNum;
+        public int destination;
+    }
     public class MyMsgType
     {
-        public static short CustomMsgType = MsgType.Highest + 1;
-        public static short Yoot = MsgType.Highest + 2;
-        public static short Battle = MsgType.Highest + 3;
-        public static short Win = MsgType.Highest + 4;
-        public static short Lose = MsgType.Highest + 5;
-        public static short Disconnect = MsgType.Highest + 10;
-    }
+        public static short MatchCancel = MsgType.Highest + 1;
+        public static short MatchSuccess = MsgType.Highest + 2;
 
-    public static void OnMessage(NetworkMessage netMsg)
-    {
-        BYMessage.MyMessage msg = netMsg.ReadMessage<BYMessage.MyMessage>();
-        Debug.Log("Message Received: " + msg.str);
+        public static short TurnStart = MsgType.Highest + 3;
+        public static short TurnEnd = MsgType.Highest + 4;
+
+        public static short YootScene = MsgType.Highest + 5;
+        public static short MoveHorse = MsgType.Highest + 6;
+
+        public static short BattleScene = MsgType.Highest + 7;
+        public static short BattleWin = MsgType.Highest + 8;
+        public static short BattleLose = MsgType.Highest + 9;
+
+
+        public static short GameWin = MsgType.Highest + 10;
+        public static short GameLose = MsgType.Highest + 11;
+
+        public static short Disconnect = MsgType.Highest + 12;
+
     }
-    public static void OnError(NetworkMessage netMsg)
-    {
-        var errorMsg = netMsg.ReadMessage<UnityEngine.Networking.NetworkSystem.ErrorMessage>();
-        Debug.Log("Error:" + errorMsg.errorCode);
-    }
+    
 }
