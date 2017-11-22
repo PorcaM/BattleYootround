a63e7e592e8e61811e5bf52d77593a7ab5a80c67
sj lee <tjdwns5122@gmail.com>
2017-11-22 20:05:13 +0900

    Implement SkillArea update position

---
+++ Assets/Script/Battle/Spell/SpellArea.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellArea : MonoBehaviour {
+
+    void Update()
+    {
+        UpdatePosition();
+    }
+    
+    private void UpdatePosition()
+    {
+        float distance = Camera.main.transform.position.y;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Vector3 rayPoint = ray.GetPoint(distance);
+        transform.position = rayPoint;
+        transform.position = new Vector3(transform.position.x, .1f, transform.position.z);
+    }
+}
