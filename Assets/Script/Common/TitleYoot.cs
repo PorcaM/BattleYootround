e2d37c3594d06c3bc881aba73927c0f3c8a695ae
sj lee <tjdwns5122@gmail.com>
2017-12-03 07:40:43 +0900

    Upgrade Title graphic

---
+++ Assets/Script/Common/TitleYoot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleYoot : MonoBehaviour {
+    private Vector3 rot;
+    private float minRot = -90.0f;
+    private float maxRot = 90.0f;
+
+	// Use this for initialization
+	void Start () {
+        transform.position += new Vector3(Random.Range(-.1f, .1f), Random.Range(-.1f, .1f), Random.Range(-.1f, .1f));
+        rot = new Vector3(Random.Range(minRot, maxRot), Random.Range(minRot, maxRot), Random.Range(minRot, maxRot));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(rot * Time.deltaTime);
+	}
+}
